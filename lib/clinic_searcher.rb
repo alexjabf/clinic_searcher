00e9c914e64d602b2bc2973db9695d2077c71946
alexjabf-newsta <103437153+alexjabf-newsta@users.noreply.github.com>
2023-04-22 18:01:13 -0600

    first commit

---
+++ lib/clinic_searcher.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "clinic_searcher/version"
+
+module ClinicSearcher
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/clinic_searcher/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ClinicSearcher
+  VERSION = "0.1.0"
+end
