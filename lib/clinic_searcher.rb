8280ccec2a7dc80b44c712be25000d8edf5bc263
alexjabf-newsta <103437153+alexjabf-newsta@users.noreply.github.com>
2023-04-22 20:14:47 -0600

    Implement ClinicSearcher module and corresponding RSpec tests
    This commit implements the ClinicSearcher module, which provides a search method for querying clinics based on zip codes and distances. It also includes error handling for missing environment variables. The accompanying RSpec tests cover various scenarios, including missing and present environment variables, ensuring the functionality works as expected.

--- lib/clinic_searcher.rb
+++ lib/clinic_searcher.rb
@@ -1,8 +1,46 @@
 # frozen_string_literal: true
 
 require_relative "clinic_searcher/version"
+require "httparty"
+require "geocoder"
 
 module ClinicSearcher
-  class Error < StandardError; end
-  # Your code goes here...
+  class Error < StandardError
+    def message
+      "Please set up the environment variables for API_ENDPOINT and API_AUTH_TOKEN." \
+        " Additionally, you may configure a RATIO_DISTANCE variable to specify the search radius."
+    end
+  end
+
+  class << self
+    def search(zipcode, distance = ratio_distance)
+      raise Error.new.message if missing_required_env_variables?
+
+      response = HTTParty.get(
+        api_endpoint,
+        headers: {"X-Auth-Token" => auth_token},
+        query: {zipcode:, distance:}
+      )
+      response.parsed_response
+    end
+
+    private
+
+    def api_endpoint
+      ENV.fetch("API_ENDPOINT", nil)
+    end
+
+    def auth_token
+      ENV.fetch("API_AUTH_TOKEN", nil)
+    end
+
+    def ratio_distance
+      ENV.fetch("RATIO_DISTANCE", 20)
+    end
+
+    def missing_required_env_variables?
+      # Check if the required environment variables are set, return true if they are missing
+      ENV["API_ENDPOINT"].nil? || ENV["API_AUTH_TOKEN"].nil?
+    end
+  end
 end
---
+++ spec/clinic_searcher_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "clinic_searcher"
+
+RSpec.describe ClinicSearcher do
+  let(:zipcode) { "12345" }
+  let(:distance) { 10 }
+
+  describe ".search" do
+    context "when required environment variables are not set" do
+      before do
+        allow(ENV).to receive(:[]).with("API_ENDPOINT").and_return(nil)
+        allow(ENV).to receive(:[]).with("API_AUTH_TOKEN").and_return(nil)
+      end
+
+      it "raises an error with an appropriate message" do
+        error_message = ClinicSearcher::Error.new.message
+        expect { described_class.search(zipcode, distance) }.to raise_error(error_message)
+      end
+    end
+
+    context "when required environment variables are set" do
+      before do
+        allow(ENV).to receive(:[]).with("API_ENDPOINT").and_return("https://example.com/api")
+        allow(ENV).to receive(:[]).with("API_AUTH_TOKEN").and_return("sample-auth-token")
+        allow(HTTParty).to receive(:get).and_return(double("response", parsed_response: "OK"))
+      end
+
+      it "returns the parsed response" do
+        expect(described_class.search(zipcode, distance)).to eq("OK")
+      end
+    end
+  end
+end
